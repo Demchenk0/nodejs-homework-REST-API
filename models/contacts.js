5f9f05e70793c689465c87d4062c9c3e90f30227
Demchenk0 <santadyr2016@gmail.com>
2022-11-18 08:24:01 +0200

    addHW2

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,76 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const path = require("path");
+const crypto = require('crypto');
 
-const getContactById = async (contactId) => {}
+const updateContact = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+const contactsPath = path.join(__dirname, "./contacts.json");
 
-const removeContact = async (contactId) => {}
+// TODO: задокументувати кожну функцію
+const listContacts = async () => {
+  const result = await fs.readFile(contactsPath)
+  return JSON.parse(result)
+}
 
-const addContact = async (body) => {}
+const getContactById = async (id) => {
+  const contacts = await listContacts()
+  const result = await contacts.find(contact => contact.id === id)
+  if (!result) {
+    return null;
+  }
+  return result;
+}
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts()
+  const newContact = {
+    id: crypto.randomInt(999).toString(),
+    name,
+    email,
+    phone,
+  }
+  contacts.push(newContact);
+  updateContact(contacts);
+  return newContact
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContactById = async (id, data) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id === id);
+
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = {
+    ...data, id
+  }
+  await updateContact(contacts)
+  return contacts[idx];
+}
+
+
+
+const removeContact = async (id) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id === id);
+
+  if (idx === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(idx, 1);
+  updateContact(contacts);
+
+  return result;
+}
+
+
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
-  updateContact,
+  removeContact,
+  updateContactById
 }
