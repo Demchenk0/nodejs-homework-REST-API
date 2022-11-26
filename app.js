011326c63ed460a228c18959753e19b22c66fe30
Demchenk0 <santadyr2016@gmail.com>
2022-11-26 20:23:09 +0200

    Home Work 3

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+require('dotenv').config()
+
+
 const contactsRouter = require('./routes/api/contacts')
+
+// const contactsRouter = require("./routes/heroku logs --tail")
+
 
 const app = express()
 
